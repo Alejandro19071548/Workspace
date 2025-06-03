d7667d63038d8af86a98f0d42ad11cf0e83abeae
Alejandro19071548 <L19071548@cdmadero.tecnm.mx>
2025-06-02 22:00:52 -0600

    Correcion de carpetas movidas

--- IJ Comunity/Proyecto_1/src/practica_3.kt
+++
@@ -1,6 +0,0 @@
-fun main() {
-    print("Ingrese la medida del lado del cuadrado:")
-    val lado = readLine()!!.toInt()
-    val perimetro = lado * 4
-    println("El perímetro del cuadrado es $perimetro")
-}
--- IJ Comunity/Proyecto_1/src/practica_5.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    print("Ingrese el sueldo del empleado:")
-    val sueldo = readln().toDouble()
-    if (sueldo > 3000) {
-        println("Debe pagar impuestos")
-    }else{
-        println("No se debe pagar impuestos :D")
-    }
-}
