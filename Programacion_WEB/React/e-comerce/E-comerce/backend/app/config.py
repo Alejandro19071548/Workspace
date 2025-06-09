9da5bdd7e916747b1b2bd85be54f859dee407ebe
Alejandro19071548 <L19071548@cdmadero.tecnm.mx>
2025-06-08 20:49:07 -0600

    Eliminar carpeta e-comerce

--- Programacion_WEB/React/e-comerce/E-comerce/backend/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "clave_secreta_segura")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "mysql+pymysql://root:Stprm123@localhost/ecomerce")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "super_jwt_secreto")
--- Programacion_WEB/React/e-comerce/E-comerce/backend/app/models/cart.py
+++
@@ -1,9 +0,0 @@
-from app.db import db
-
-class CartItem(db.Model):
-    __tablename__ = 'cart_items'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
-    cantidad = db.Column(db.Integer, default=1)
